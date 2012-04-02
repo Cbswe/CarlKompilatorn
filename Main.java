42a7b594b02cee3fe8f4db5db817e817706bdfdd
Carl <cbswe@bredband.net>
2012-04-02 17:25:29 +0200

    hej

--- Main.java
+++ Main.java
@@ -7,23 +7,48 @@ import lexAndParse.node.* ;
 import lexAndParse.analysis.*;
   
 import java.io.* ; 
+
   
-public class Main { 
+public class Main {
+	
+	 public static void main(String[] args) { 
+	      if (args.length > 0) { 
+	         try { 
+	            /* Form our AST */ 
+	            Lexer lexer = new Lexer (new PushbackReader( 
+	               new FileReader(args[0]), 1024)); 
+	            Parser parser = new Parser(lexer); 
+	            Start ast = parser.parse() ; 
+	  
+	            /* Get our Interpreter going. */ 
+	            //Interpret interp = new Interpret () ; 
+	            ast.apply(new DepthFirstAdapter()) ; 
+	            System.out.println("All OK");
+	         } 
+	         catch (Exception e) { 
+	            System.out.println (e) ; 
+	         } 
+	      } else { 
+	         System.err.println("usage: java simpleAdder inputFile"); 
+	         System.exit(1); 
+	      } 
+	   } 
+	/*
    public static void main(String[] args) { 
          try { 
-            /* Form our AST */ 
+            // Form our AST / 
             Lexer lexer = new Lexer (new PushbackReader( 
                new InputStreamReader(System.in), 1024)); 
             Parser parser = new Parser(lexer); 
             Start ast = parser.parse() ; 
   
-            /* Get our Interpreter going. */
+            // Get our Interpreter going. /
             
-            ast.apply(new DepthFirstAdapter ()) ;
+            ast.apply(new Interpret ()) ;
             System.out.println("Checks out OK");
          } 
          catch (Exception e) { 
             System.out.println (e) ; 
          }  
-   } 
+   } */
 }
